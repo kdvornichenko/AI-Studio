7fe27fd4b166698a47c889b07e1fc4ef1199d941
kdvornichenko <gglade999@gmail.com>
2022-06-23 00:05:21 +0600

    Project created

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./*.{html,js}'],
+	theme: {
+		backgroundColor: {
+		},
+		colors: {
+		},
+		extend: {
+			boxShadow: {
+			},
+			backgroundImage: {
+			},
+			transitionTimingFunction: {
+			},
+		},
+	},
+	plugins: [],
+}
