61942b9330aab027242c12a278a65ef93f0d742b
kdvornichenko <gglade999@gmail.com>
2022-06-23 23:41:17 +0600

    First screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,17 @@ module.exports = {
 			halvar: 'Halvar Mittelschrift',
 		},
 		backgroundColor: {
-			main: '#FBFBFD',
 			blue: '#4061F8',
 			white: '#FBFBFD',
 			black: '#151516',
-			mobMenu: '#E9EEF3',
+			lightGrey: '#E9EEF3',
 			burgerBg: '#bfecff',
 		},
 		colors: {
 			white: '#FBFBFD',
 			black: '#151516',
 			blue: '#4061F8',
+			serviceH4: '#9A52FD',
 		},
 		extend: {
 			boxShadow: {},
